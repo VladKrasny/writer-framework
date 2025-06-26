d852ac1d52aa719ef6599ee27da1b9b758ff1f01
Alexandre Rousseau <alexandre@rsseau.fr>
2025-06-26 13:49:53 +0200

    feat(ui): introduce new auto-complete dropdown - WF-500

--- src/ui/src/core/index.ts
+++ src/ui/src/core/index.ts
@@ -1,6 +1,14 @@
 /* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { computed, readonly, ref, Ref, shallowRef, toRaw } from "vue";
+import {
+	computed,
+	readonly,
+	ref,
+	Ref,
+	ShallowRef,
+	shallowRef,
+	toRaw,
+} from "vue";
 import {
 	AbstractTemplate,
 	Component,
@@ -54,6 +62,7 @@ export function generateCore() {
 	});
 	const components: Ref<ComponentMap> = ref({});
 	const userFunctions: Ref<UserFunction[]> = ref([]);
+	const userStateInitial: ShallowRef<Record<string, unknown>> = ref({});
 	const userState: Ref<Record<string, any>> = ref({});
 	let webSocket: WebSocket;
 	const syncHealth: Ref<"idle" | "connected" | "offline" | "suspended"> =
@@ -117,6 +126,7 @@ export function generateCore() {
 		mode.value = initData.mode;
 		components.value = initData.components;
 		userState.value = initData.userState;
+		userStateInitial.value = initData.userState;
 		collateMail(initData.mail);
 		sessionId = initData.sessionId;
 		sessionTimestamp.value = new Date().getTime();
@@ -861,6 +871,7 @@ export function generateCore() {
 		getContainableTypes,
 		sessionTimestamp: readonly(sessionTimestamp),
 		userState: readonly(userState),
+		userStateInitial: readonly(userStateInitial),
 		isChildOf,
 		featureFlags: readonly(featureFlags),
 		getWebSocket,
